f03d6c829cdf087be0cd2f722ad23f01dfa894fc
kenedy <kenedy@myhechar.pro>
2022-11-07 18:51:44 +0300

    initial commit

---
+++ app/src/main/java/com/ramanie/animatedspalshscreen/Navigation.kt
@@ -0,0 +1,17 @@
+package com.ramanie.animatedspalshscreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "splash_frag", builder = {
+        composable("", content = {
+
+        })
+    })
+}
---
+++ app/src/main/java/com/ramanie/animatedspalshscreen/RoutingConstants.kt
@@ -0,0 +1,7 @@
+package com.ramanie.animatedspalshscreen
+
+enum class Routes(val route: String){
+    SPLASH_SCREEN_TO_HOME_FRAG("splash_to_home"),
+    HOME_FRAG_TO_SPLASH_SCREEN("home_to_splash"),
+
+}
